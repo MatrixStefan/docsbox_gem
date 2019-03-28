8a63dd6d13e9e1f7d4b8b08ffa31f45ce4e85625
MatrixStefan <stefan.ritchie@thisismatrix.com>
2019-03-28 10:52:23 +0000

    Initial initializer

--- lib/docsbox.rb
+++ lib/docsbox.rb
@@ -4,4 +4,17 @@ module DocsBox
             puts 'pong'
         end
     end
+
+    class Inbox
+        def initialize(box, multi_attachment, doc_model, single_attachment)
+            box.multi_attachment.attachments[-params[box_symbol.to_sym][multi_attachment.to_sym].count..-1].each do |new|
+                doc_model.create(box_id: box.id,
+                           name: new.filename,
+                           author_id: 1,
+                           read: false,
+                           uploader_id: 1).single_attachment.attach(new.blob)
+                new.purge_later
+            end
+        end
+    end
 end
